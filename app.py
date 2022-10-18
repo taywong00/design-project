44002c3a6eb48fc94567ec7d872f230015d46e8f
taywong00 <taywong00@gmail.com>
2022-10-18 02:42:37 -0400

    some more starter pages

--- app.py
+++ app.py
@@ -21,12 +21,23 @@ def sas_about():
 # Share A Swipe Log In
 @app.route("/sas_login")
 def sas_login():
+    # return render_template("/sas_login.html")
     return redirect("/sas_home") # ignore login for now, assuming nyu mfa?
 
 # Share A Swipe Home
 @app.route("/sas_home")
 def sas_home():
     return render_template("sas_home.html")
+
+# Share A Swipe Register
+@app.route("/sas_register")
+def sas_register():
+    return render_template("sas_register.html")
+
+# Share A Swipe Status
+@app.route("/sas_status")
+def sas_status():
+    return render_template("sas_status.html")
 
 
 if __name__ == "__main__":
